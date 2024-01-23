02259668764646fa70b5d0b826f2ffed86c9db30
Acer <Acer@LAPTOP-9SISV8A7>
2024-01-23 17:59:20 +0100

    Aggiungere i file di progetto.

---
+++ casa 2/Auto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace casa_2
+{
+    internal class Auto
+    {
+        public string Modello { get; set; }
+    }
+}
